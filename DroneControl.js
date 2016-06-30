e55962a2dba77ed32b41383bb6f531d09b7f0baf
mhasin <michael.hasin@intel.com>
2016-06-30 14:50:22 +0300

    Fine tunning of controls, and fixed takeoff

--- DroneControl.js
+++ DroneControl.js
@@ -5,16 +5,17 @@ var control = arDrone.createUdpControl();
 //var pcmd = {}; // controls up/left/right/spin
 
 
-
-
-// A Point protoype
+/**
+ * A Point prototype
+ * to be used for drone navigation.
+ */
 function Point(x, y, z) {
     this.x = x;
     this.y = y;
     this.z = z;
     // Checks if a given point truncated is equal this
-    this.equals = function (otherLoc) {
-        return this.truncEq(this.x, otherLoc.x) && this.truncEq(this.y, otherLoc.y) && this.truncEq(this.z, otherLoc.z);
+    this.EqualsEpsilon = function (otherLoc,eps) {
+        return this.eqEps(this.x, otherLoc.x,eps) && this.eqEps(this.y, otherLoc.y,eps) && this.eqEps(this.z, otherLoc.z,eps);
     };
 
     this.set = function (x, y, z) {
@@ -23,45 +24,50 @@ function Point(x, y, z) {
         this.z = z;
     };
     // Checks if 2 given values truncated are equal
-    this.truncEq = function (a, b) {
-        return (a | 0) == (b | 0);
+    this.eqEps = function (a, b,eps) {
+        return Math.abs(a-b)<eps;
     };
 }
 
-
-// Handles all drone flight control
-// x+: right
-// y+: front
-// z+: up
+/**
+ * Handles all drone flight control
+ * Works on simple 3D coordinate system as follows:
+ * x+: right
+ * y+: front
+ * z+: up
+ */
 var droneControl =
 {
-    //Constants
-    //Minimum interval between every update
-    MINIMUM_INTERVAL: 30,
-    // Increment value to location on every iteration
-    MOVE_QUANTA: 0.2,
-    //Members
-    IntervalId: 0,
-    pcmd: {},
-    ref: {},
-    currentLocation: new Point(0, 0, 0),
-    targetLocation: new Point(0, 0, 0),
+    /* Constants */
+    MINIMUM_INTERVAL: 30, //Minimum interval between every update
+    MOVE_QUANTA: 0.04, // Increment value to location on every iteration
+    FLIGHT_EPS : 0.02, //Flight Accuracy Epsilon
 
-    // Inits Drone control
+    /* Members */
+    IntervalId: 0, //we need this in order to terminate main loop.
+    pcmd: {}, // up,left,front control struct
+    ref: {}, // fly,emergency control struct
+    currentLocation: new Point(0, 0, 0), // Current drone location .
+    targetLocation: new Point(0, 0, 0),  // Target location (location drone needs to go to).
+
+    /**
+     * Init Drone control main loop and set emergency parameters
+     */
     Init: function () {
-        droneControl.ref.emergency=false;
+        droneControl.ref.emergency=true;
         droneControl.IntervalId = setInterval(this.MainLoop, this.MINIMUM_INTERVAL);
+        setTimeout(function(){console.log("Emergency=false");droneControl.ref.emergency=false;droneControl.Takeoff()},1000);
     },
 
 
     /**
      * Calculates the forces required to fly from source (point) to dest (point)
-     * @return {number}
+     * @return {number} force "vector"
      */
     CalcForce: function (diff) {
+        if(Math.abs(diff) < droneControl.FLIGHT_EPS) return 0;
         if (diff > 0) return 1;
-        if (diff < 0) return -1;
-        return 0;
+        else return -1;
     },
 
 
@@ -79,6 +85,9 @@ var droneControl =
         droneControl.ref.fly=false;
     },
 
+    /**
+     * Terminates the main loop and lands drone.
+     */
     Terminate: function () {
         console.log("Terminating loop intID: ");
         this.Land();
@@ -86,7 +95,9 @@ var droneControl =
     },
 
 
-
+    /**
+     * Updates on every iteration drone location according to forces
+     */
     LocationUpdater: function () {
         if(droneControl.pcmd== null) return;
         this.currentLocation.x += (droneControl.MOVE_QUANTA * -droneControl.pcmd.left);
@@ -94,8 +105,11 @@ var droneControl =
         this.currentLocation.z += (droneControl.MOVE_QUANTA * droneControl.pcmd.up);
     },
 
+    /**
+     * Every iteration updates flight forces to send to drone
+     **/
     FlightUpdater: function () {
-        if (!this.currentLocation.equals(this.targetLocation)) {
+        if (!this.currentLocation.EqualsEpsilon(this.targetLocation,droneControl.FLIGHT_EPS)) {
             if (!droneControl.pcmd) droneControl.pcmd = {};
             droneControl.pcmd.left =  this.CalcForce(this.currentLocation.x - this.targetLocation.x);
             droneControl.pcmd.front = this.CalcForce(  this.targetLocation.y-this.currentLocation.y);
@@ -105,154 +119,42 @@ var droneControl =
         }
     },
 
+    /**
+     * Main loop , this function is called from within setInterval and is invoked
+     * every MOVE_QUANTA milliseconds
+     **/
     MainLoop: function () {
         droneControl.FlightUpdater();
         droneControl.LocationUpdater();
         var p = (droneControl.pcmd==null)? {front:0,left:0,up:0} : droneControl.pcmd;
         console.log("CLOC["+droneControl.currentLocation.x+","+droneControl.currentLocation.y+","+droneControl.currentLocation.z+"] | PCMD["+(-p.left)+","+(p.front)+","+(p.up)+"]");
-        control.ref(this.ref);
-        control.pcmd(this.pcmd);
+        control.ref(droneControl.ref);
+        control.pcmd(droneControl.pcmd);
         control.flush();
     }
 
 };
 
+
+//////////////////////// TEST CODE ////////////////////
 function p(msg){
     console.log(msg);
 }
 
 function Test() {
     droneControl.Init();
-    droneControl.Takeoff();
-    var QUANT=300;
-    setTimeout(function () {droneControl.FlyTo(0,1,0);p("TO 0,1,0");},QUANT);
-    setTimeout(function () {droneControl.FlyTo(1,1,0);p("TO 1,1,0");},QUANT*2);
-    setTimeout(function () {droneControl.FlyTo(1,0,0);p("TO 1,0,0");},QUANT*3);
-    setTimeout(function () {droneControl.FlyTo(0,0,0);p("TO 0,0,0");},QUANT*4);
-    setTimeout(function () {droneControl.Terminate();},QUANT*5);
+   // droneControl.Takeoff();
+    var QUANT=2500;
+    var START=3000;
+
+    setTimeout(function () {droneControl.FlyTo(1,1,1);p("TO 0,1,0");},START+QUANT);
+    setTimeout(function () {droneControl.FlyTo(1,1,0);p("TO 1,1,0");},START+QUANT*2);
+    setTimeout(function () {droneControl.FlyTo(0,1,0);p("TO 1,0,0");},START+QUANT*3);
+    setTimeout(function () {droneControl.FlyTo(0,0,0);p("TO 0,0,0");},START+QUANT*4);
+    setTimeout(function () {droneControl.Terminate();},START+QUANT*5);
+    setTimeout(function () {process.exit();},START+QUANT*6);
+
 }
 
 Test();
 
-
-// console.log('Recovering from emergency mode if there was one ...');
-// ref.emergency = true;
-// setTimeout(function () {
-//     console.log('Takeoff ...');
-//
-//     ref.emergency = false;
-//     ref.fly = true;
-//
-// }, 1000);
-//
-// setTimeout(function () {
-//     console.log('Turning left .1..');
-//     FlyTo(1, 1, 1);
-//     tmp = true;
-//     //pcmd.clockwise = 0.5;
-//     pcmd.left = 0;
-//     pcmd.front = 1;
-//     // pcmd.up =1;
-// }, 6000);
-//
-// var tmp = false;
-//
-// function FlyTo(x, y, z) {
-//     pcmd.front = 1;
-//     pcmd.right = 0;
-//     //pcmd.up = 1;
-// }
-//
-// function FlyBack(x, y, z) {
-//     //pcmd.front = -1;
-//     // pcmd.right = -1;
-//     //pcmd.up = -1;
-// }
-//
-// setTimeout(function () {
-//     console.log('no turn');
-//
-//     //pcmd = {};
-// }, 8000);
-//
-// setTimeout(function () {
-//     console.log('Turning right ...');
-//
-//     //pcmd.clockwise = 0.5;
-//     //pcmd.right = 0.1;
-// }, 7000);
-//
-//
-// setTimeout(function () {
-//     console.log('no turn');
-//     ref.fly = false;
-//
-// }, 14000);
-//
-// /*
-//  setTimeout(function() {
-//  console.log('Landing ...');
-//
-//  ref.fly = false;
-//  pcmd = {};
-//  }, 8000);
-//  */
-// var ctr = 0;
-//
-// setInterval(function () {
-//     /*
-//      if(inTime){
-//      if(up){
-//
-//      }
-//      if(right){
-//
-//      }
-//      if(front){
-//
-//      }
-//      updateLoc();
-//      }
-//      */
-//     if (ctr == 18) {
-//         pcmd = null;
-//     }
-//     if (ctr == 20) {
-//
-//         pcmd = {};
-//         console.log("RIGHT");
-//         pcmd.front = 0;
-//         pcmd.left = -1;
-//     }
-//     if (ctr == 38) {
-//         pcmd = null;
-//     }
-//
-//     if (ctr == 40) {
-//
-//         pcmd = {};
-//         console.log("BACK");
-//         pcmd.front = -1;
-//         pcmd.left = 0;
-//     }
-//     if (ctr == 58) {
-//         pcmd = null;
-//     }
-//     if (ctr == 60) {
-//         pcmd = {};
-//         console.log("LEFT");
-//         pcmd.front = 0;
-//         pcmd.left = 1;
-//     }
-//
-//     if (ctr == 80) {
-//         console.log("bbbb");
-//         pcmd = null;
-//     }
-//
-//
-//     control.ref(ref);
-//     control.pcmd(pcmd);
-//     control.flush();
-//     if (tmp) ctr++;
-// }, 30);
--- mkT2.js
+++ mkT2.js
@@ -1,3 +1,126 @@
 var arDrone = require('ar-drone');
-var client  = arDrone.createClient();
-client.createRepl();
+var control = arDrone.createUdpControl();
+
+var ref={};
+var pcmd={};
+console.log('Recovering from emergency mode if there was one ...');
+ref.emergency = true;
+setTimeout(function () {
+    console.log('Takeoff ...');
+
+    ref.emergency = false;
+    ref.fly = true;
+
+}, 1000);
+
+setTimeout(function () {
+    console.log('Turning left .1..');
+    FlyTo(1, 1, 1);
+    tmp = true;
+    //pcmd.clockwise = 0.5;
+    pcmd.left = 0;
+    pcmd.front = 1;
+    // pcmd.up =1;
+}, 6000);
+
+var tmp = false;
+
+function FlyTo(x, y, z) {
+    pcmd.front = 1;
+    pcmd.right = 0;
+    //pcmd.up = 1;
+}
+
+function FlyBack(x, y, z) {
+    //pcmd.front = -1;
+    // pcmd.right = -1;
+    //pcmd.up = -1;
+}
+
+setTimeout(function () {
+    console.log('no turn');
+
+    //pcmd = {};
+}, 8000);
+
+setTimeout(function () {
+    console.log('Turning right ...');
+
+    //pcmd.clockwise = 0.5;
+    //pcmd.right = 0.1;
+}, 7000);
+
+
+setTimeout(function () {
+    console.log('no turn');
+    ref.fly = false;
+
+}, 14000);
+
+/*
+ setTimeout(function() {
+ console.log('Landing ...');
+
+ ref.fly = false;
+ pcmd = {};
+ }, 8000);
+ */
+var ctr = 0;
+
+setInterval(function () {
+    /*
+     if(inTime){
+     if(up){
+
+     }
+     if(right){
+
+     }
+     if(front){
+
+     }
+     updateLoc();
+     }
+     */
+    if (ctr == 18) {
+        pcmd = null;
+    }
+    if (ctr == 20) {
+
+        pcmd = {};
+        console.log("RIGHT");
+        pcmd.front = 0;
+        pcmd.left = -1;
+    }
+    if (ctr == 38) {
+        pcmd = null;
+    }
+
+    if (ctr == 40) {
+
+        pcmd = {};
+        console.log("BACK");
+        pcmd.front = -1;
+        pcmd.left = 0;
+    }
+    if (ctr == 58) {
+        pcmd = null;
+    }
+    if (ctr == 60) {
+        pcmd = {};
+        console.log("LEFT");
+        pcmd.front = 0;
+        pcmd.left = 1;
+    }
+
+    if (ctr == 80) {
+        console.log("bbbb");
+        pcmd = null;
+    }
+
+
+    control.ref(ref);
+    control.pcmd(pcmd);
+    control.flush();
+    if (tmp) ctr++;
+}, 30);
