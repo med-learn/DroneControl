bcb284911b90cc87611e1ffa6865a1fd6fe9f032
mike <mike.hsn@gmail.com>
2016-07-06 01:09:11 +0300

    :convert Rs MAnager to use ECMA6 classes

--- js/modules/MainControllerModule.js
+++ js/modules/MainControllerModule.js
@@ -1,5 +1,5 @@
 //const UIModule = require('./js/modules/UIModule.js');
-//const RSModule = require('./js/modules/RSModule.js');
+const RSModule = require('./js/modules/RSModule.js');
 //const DroneModule = require('./js/modules/DroneModule.js');
 
 class MainController{
