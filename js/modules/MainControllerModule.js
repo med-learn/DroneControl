99dfd8e879e73b2bc3ea0bef38b8c3fbd1bc788f
nparisia <nir.parisian@intel.com>
2016-08-29 17:18:12 +0300

    add implemention for ui drawpos method

--- js/modules/MainControllerModule.js
+++ js/modules/MainControllerModule.js
@@ -45,14 +45,14 @@ class MainController{
 
     onPointUpdate(x,y,z){
 
-        var cx = (1 - x) * window.innerWidth;
-        var cy = y * window.innerHeight;
-        mcRef.cursorElem.style.top = (cy - mcRef.cursorElem.clientHeight/2) + "px";
-        mcRef.cursorElem.style.left = (cx - mcRef.cursorElem.clientHeight/2) + "px";
-
-        mcRef.cursorElem.style.width = ((1-z)*400)+"px"; //(z*mcRef.cursorElem.style.width)+"px";
-        mcRef.cursorElem.style.height = ((1-z)*400)+"px";//(z*mcRef.cursorElem.style.height)+"px";
-
+        //var cx = (1 - x) * window.innerWidth;
+        //var cy = y * window.innerHeight;
+        //mcRef.cursorElem.style.top = (cy - mcRef.cursorElem.clientHeight/2) + "px";
+        //mcRef.cursorElem.style.left = (cx - mcRef.cursorElem.clientHeight/2) + "px";
+        //
+        //mcRef.cursorElem.style.width = ((1-z)*400)+"px"; //(z*mcRef.cursorElem.style.width)+"px";
+        //mcRef.cursorElem.style.height = ((1-z)*400)+"px";//(z*mcRef.cursorElem.style.height)+"px";
+        mcRef.uiCtrl.drawPos(x,y,z);
 
         //p("point x:"+x);
         //mcRef.droneCtrl.FlyTo(2*(1-2*x),2*(1-2*y),1.2*(z-1));
